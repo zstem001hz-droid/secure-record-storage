5a506ccbe922b46fb4e878b705dd701e8873a4ca
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-21 16:03:11 -0400

    feat: add POST create note route to note routes

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -9,9 +9,22 @@ router.use(authMiddleware);
 // GET /api/notes = Get all notes for a logged-in user
 router.get("/", async (requestAnimationFrame, res) => {
   try {
-    const notes = await Note.find({});
+    const notes = await Note.find({ user: req.user._id });
     res.json(notes);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+// POST /api/notes - Create a new note
+router.post("/", async (req, res) => {
+  try {
+    const note = await Note.create({
+      ...req.body,
+      user: req.user._id,
+    });
+    res.status(201).json(note);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
