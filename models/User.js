dbde72a0d252001ce359620289c58eaab9470db6
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-21 17:20:40 -0400

    feat: add User model with bcrypt password hashing

---
+++ models/User.js
@@ -0,0 +1,41 @@
+// Requirements
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// User Schema
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+});
+
+// Pre-save hook to hash password
+userSchema.pre("save", async function () {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+});
+
+// Instance method to verify password on login
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
