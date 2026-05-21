6ef9c2665683598619ca0d3acc873740e4179a33
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-21 15:43:29 -0400

    feat: add GET all notes route to note routes

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -5,3 +5,13 @@ const { authMiddleware } = require("../../utils/auth");
 
 // Apply authMiddleware to all routes in this file
 router.use(authMiddleware);
+
+// GET /api/notes = Get all notes for a logged-in user
+router.get("/", async (requestAnimationFrame, res) => {
+  try {
+    const notes = await Note.find({});
+    res.json(notes);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
